77976c0112296e4add65cefd040f856e459466f2
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-07 23:56:25 +0530

    learn

---
+++ Lecture 9_10_11/Array1.cpp
@@ -0,0 +1,19 @@
+//for the input and output from the user
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, arr[100];
+    cout<<"Enter the size of Array: "; // for the message what to do by the user
+    cin>>n;
+    for(int i=0; i<n; i++){    // for input
+        cout<<"Index "<<i<<": ";
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){ // for output
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+
